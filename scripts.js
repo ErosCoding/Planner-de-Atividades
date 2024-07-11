22409e09e02e5784302fdfced6bf0703a6945cc9
Eros <sickoferos@gmail.com>
2024-07-11 01:18:22 -0300

    Update corrigindo erros

--- scripts.js
+++ scripts.js
@@ -1,48 +1,47 @@
 // object {}
 const atividade = {
     nome: "Almoço",
-    data: new Date("2024-07-10 10:00"),
-    finalizada: false
-}
-
-// lista, array, vetor
-const atividades = [
+    data: new Date("2024-07-08 10:00"),
+    finalizada: true
+  }
+  
+  // lista, array, vetor []
+  const atividades = [
     atividade,
     {
-        nome: 'Academia em grupo',
-        data: new Date("2024-07-10 12:00"),
-        finalizada: false
+      nome: 'Academia em grupo',
+      data: new Date("2024-07-09 12:00"),
+      finalizada: false
     },
     {
-        nome: 'Gaming Session',
-        data: new Date("2024-07-10 16:00"),
-        finalizada: true 
-    }
-]
-
-// arrow function
-const criarItemDeAtividade = (atividade) => {
-
+      nome: 'Gamming session',
+      data: new Date("2024-07-09 16:00"),
+      finalizada: true
+    },
+  ]
+  
+  // arrow function
+  const criarItemDeAtividade = (atividade) => {
+  
     let input = '<input type="checkbox" '
-
+  
     if(atividade.finalizada) {
-        input += 'checked'
+      input += 'checked'
     }
-
-    input += input + '>'
-    
+  
+    input += '>'
+  
     return `
-     <div>
-        ${input}
-        <span>${atividade.nome}</span>
-        <time>${atividade.data}</time>
+    <div>
+      ${input}
+      <span>${atividade.nome}</span>
+      <time>${atividade.data}</time>
     </div>
     `
-}
-const section = document.querySelector('section')
-
-for(let atividade of atividades) {
+  }
+  
+  
+  const section = document.querySelector('section')
+  for(let atividade of atividades) {
     section.innerHTML += criarItemDeAtividade(atividade)
-}
-
-
+  }
